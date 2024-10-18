fff2152f213e8b9a982e5e611ea84df791050052
yogesh gurjar <yogesh67gurjar@gmail.com>
2024-10-19 00:10:14 +0530

    init

---
+++ app/src/main/java/com/yogesh/navigationcomponentdemo/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.yogesh.navigationcomponentdemo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.yogesh.navigationcomponentdemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+lateinit var activityMainBinding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+    }
+}
